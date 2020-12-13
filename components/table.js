1f8e761811f1766f13563e527442fb98f2dbf4b3
mohamedeliwa <mohamed.av11@gmail.com>
2020-12-13 22:05:01 +0200

    displaying arrows functionality

--- components/table.js
+++ components/table.js
@@ -1,29 +1,94 @@
+import { useState } from "react";
 import styles from "../styles/table.module.css";
-
+import { AiOutlineCaretDown, AiOutlineCaretUp } from "react-icons/ai";
 const Table = () => {
+  const [sorting, setSorting] = useState({
+    element: "date",
+    param: "createdAt",
+    direction: "desc",
+  });
+
+  const handleSorting = (e) => {
+    e.preventDefault();
+    if (e.target.id === sorting.element) {
+      setSorting({
+        ...sorting,
+        direction: sorting.direction === "asc" ? "desc" : "asc",
+      });
+      let text = e.target.innerText.split(" ");
+      text[1] = text[1] === "v" ? "^" : "v";
+      e.target.innerText = text.join(" ");
+    } else {
+      const oldElement = document.querySelector(`#${sorting.element}`);
+      oldElement.innerText = oldElement.innerText.split(" ")[0];
+      setSorting({
+        element: e.target.id,
+        param: e.target.dataset.param,
+        direction: "desc",
+      });
+      let text = e.target.innerText.split(" ");
+      text[1] = text[1] === "v" ? "^" : "v";
+      e.target.innerText = text.join(" ");
+    }
+  };
   return (
     <div className={styles.container}>
       <table className={styles.table}>
         <caption className={styles.caption}>Users' Data</caption>
         <thead>
           <tr>
-            <th className={styles.th} scope="col">
-              Index ^
+            <th
+              id="index"
+              className={styles.th}
+              scope="col"
+              onClick={handleSorting}
+            >
+              Index
             </th>
-            <th className={styles.th} scope="col">
-              First Name
+            <th
+              id="firstName"
+              className={styles.th}
+              data-param="first_name"
+              scope="col"
+              onClick={handleSorting}
+            >
+              FirstName
             </th>
-            <th className={styles.th} scope="col">
-              Last Name
+            <th
+              id="lastName"
+              data-param="last_name"
+              className={styles.th}
+              scope="col"
+              onClick={handleSorting}
+            >
+              LastName
             </th>
-            <th className={styles.th} scope="col">
+            <th
+              id="email"
+              data-param="email"
+              className={styles.th}
+              scope="col"
+              onClick={handleSorting}
+            >
               Email
             </th>
-            <th className={styles.th} scope="col">
+            <th
+              id="gender"
+              data-param="gender"
+              className={styles.th}
+              scope="col"
+              onClick={handleSorting}
+            >
               Gender
             </th>
-            <th className={styles.th} scope="col">
-              Created Date
+            <th
+              id="date"
+              data-param="createdAt"
+              className={styles.th}
+              scope="col"
+              onClick={handleSorting}
+            >
+              CreatedDate v
             </th>
           </tr>
         </thead>
